8c33f702411f9551b0d507ad681e29c51dd9955f
John Nolette <jnolette@transparent.com>
2019-02-22 13:50:18 -0500

    Adding travis and jest config

--- src/tag.js
+++ src/tag.js
@@ -1,20 +1,24 @@
-import {Constants} from './constants'
+import { Constants } from "./constants"
 
 export default function Tag(opts) {
-    const self = this
+  const self = this
 
-    if (self.root instanceof HTMLInputElement && (self.root.type == 'text' || self.root.type == 'password')) {
-        self.root.addEventListener('keyup', function(e) {
-            const value = self.root.value.replace(/\s/g, '')
-            if (value.length > Constants.minPasswordLength) {
-                self.passwordKnown(value)
-                    .then(matched => {
-                        self.parent.trigger('passwordKnown', matched)
-                    })
-                    .catch(err => {
-                        self.parent.trigger('passwordKnown', false)
-                    })
-            }
-        })
-    }
+  if (
+    self.root instanceof HTMLInputElement &&
+    (self.root.type == "text" || self.root.type == "password")
+  ) {
+    self.root.addEventListener("keyup", function(e) {
+      const value = self.root.value.replace(/\s/g, "")
+      if (value.length > Constants.minPasswordLength) {
+        self
+          .passwordKnown(value)
+          .then(matched => {
+            self.parent.trigger("passwordKnown", matched)
+          })
+          .catch(err => {
+            self.parent.trigger("passwordKnown", false)
+          })
+      }
+    })
+  }
 }
