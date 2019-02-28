3828ef1a1a435497c14bb95b31a542ca909127f3
John Nolette <jnolette@transparent.com>
2019-02-27 22:21:12 -0500

    Resolving mixin logic

--- src/mixin.js
+++ src/mixin.js
@@ -3,15 +3,19 @@ import { Constants } from './constants'
 
 export function Mixin(password) {
   const hashed = sha1(password)
-  const prefix = hashed.slice(0, Constants.slizeSize)
-  const suffix = hashed.slice(Constants.sliceSize)
+  const prefix = hashed.toString().slice(0, Constants.sliceSize)
+  const suffix = hashed.slice(Constants.sliceSize).toUpperCase()
   return new Promise(function(resolve, reject) {
     fetch(`${Constants.apiUri}/${prefix}`)
       .then(response => {
         response.text().then(list => {
-          resolve(
-            list.split('\n').find(p => p.split(':')[0] == suffix) ? true : false
-          )
+          const result = list.split('\n').find(p => p.split(':')[0] == suffix)
+          resolve({
+            matched: result ? true : false,
+            count: result
+              ? parseInt(result.split(':')[1].replace('\\r', ''))
+              : 0
+          })
         })
       })
       .catch(() => {
